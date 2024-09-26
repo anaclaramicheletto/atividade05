4f3b2d9a9dbadbc2f5d8e5e0711bfb54bb00cfb2
unknown <analetto10@gmail.com>
2024-09-26 15:20:23 -0300

    feat: create delete emotion method

---
+++ src/routes/emocoes.routes.js
@@ -0,0 +1,103 @@
+import { Router } from "express"
+
+const emocoesRoutes = Router()
+
+let emocoes = [
+  {
+    id: 1,
+    nome: "Nojinho",
+    cor: "Verde"
+  },
+  {
+    id: 2,
+    nome: "Alegria",
+    cor: "Amarelo"
+  },
+  {
+    id: 3,
+    nome: "Raiva",
+    cor: "Vermelho"
+  }
+]
+
+//rota para buscar todas as emoções
+emocoesRoutes.get("/",(req, res ) => {
+  return res.status(200).send(emocoes)
+})
+
+//Rota para criar uma nova emoção
+emocoesRoutes.post("/", (req, res) => {
+  const {nome, cor } = req.body
+  const novaEmocao = {
+    id: emocoes.length + 1,
+    nome: nome,
+    cor: cor
+  }
+  emocoes.push(novaEmocao)
+  return res.status(201).send ( emocoes )
+});
+
+//Rota para buscar uma emoção pelo id
+emocoesRoutes.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  //console.log(id);
+  const emocao = emocoes.find((emotion) => emotion.id == id )
+
+  if (!emocao) {
+    return res.status(404).send({
+      message: "Emoção não encontrada!",
+    });
+  }
+
+  return res.status(200).send({
+    message: "Emoção encontrada",
+    emocao,
+  });
+});
+
+ 
+emocoesRoutes.put("/:id", (req, res) => {
+  const { id } = req.params;
+
+  const emocao = emocoes.find((emotion) => emotion.id == id )
+
+  if (!emocao) {
+    return res.status(404).send({
+      message: "Emoção não encontrada!",
+    });
+  }
+
+
+const {nome, cor } = req.body;
+emocao.nome = nome;
+emocao.cor = cor;
+
+return res.status(200).send({
+  message: "Emoção atualizada",
+  emocao,
+});
+});
+
+emocoesRoutes.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  const emocao = emocoes.find((emotion) => emotion.id == id )
+
+  if (!emocao) {
+    return res.status(404).send({
+      message: "Emoção não encontrada!",
+    });
+  }
+
+  emocoes = emocoes.filter((emotion) => emotion.id != id );
+
+
+  return res.status(200).send({
+    message: "Emoção deletada!",
+    emocao,
+  });
+});
+
+
+export default emocoesRoutes
--- src/server.js
+++ src/server.js
@@ -1,79 +1,14 @@
-import express from "express";
-import { config } from "dotenv";
+import express from "express"
+import { config } from "dotenv"
+import routes from "./routes/index.routes.js"
 
-config();
+config()
 
 const serverPort = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
-
-const emocoes = [
-        {
-       id:1,
-       nome:"Nojinho",
-       cor:"verde"         
-        },
-        {
-          id:2,
-          nome:"amor",
-          cor: "vermelho"
-        },
-{
-          id:3,
-          nome:"raiva",
-          cor: "vermelho"
-        }
-      ]
-
-      const personagem = [
-        {
-         id: 100,
-         nome:"totoro",
-         studio: "ghibli",
-         vivo: true
-        },
-        {
-         id:101,
-         nome:"moana",
-         studio: "marvel",
-         vivo: true
-        },
-        {           
-         id:102,
-         nome:"Barbie",
-         studio: "mattel",
-         vivo: true
-     }
-]
-
-app.get("/", (req, res) => {
-  return res.status(200).json({ message: "Hello, World!" });
-});
-
-app.get("/2tds2", (req, res) => {
-  return res.status(200).json({ message: "Hello, 2tds2!" });
-});
-
-app.post("/emocoes", (req, res) => {
-  const{nome, cor } = req.body
-  const novaEmocao = {
-    id: emocoes.length + 1,
-    nome: nome,
-    cor: cor 
-  }
-
-  emocoes.push(novaEmocao)
-  return res.status(200)
-  .send(emocoes)
-  return res.status(200).send(emocoes);
-
-});
-
-app.get("/personagens", (req, res) => {
-  return res.status(200).send(personagem);
-});
-
+app.use(routes);
 
 app.listen(serverPort, () => {
   console.log(`⚡ Server started on http://localhost:${serverPort}`);
